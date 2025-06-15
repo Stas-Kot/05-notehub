64f880025bad8f4c3c40b6d0180a5870300c21de
Stanislav Kotelnykov <crock@Crock-MacBook-Pro.local>
2025-06-15 22:48:38 +0300

    do notes app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,54 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+import css from "./App.module.css";
+import { fetchNotes } from "../../services/noteService";
+import NoteList from "../NoteList/NoteList";
+import NoteModal from "../NoteModal/NoteModal";
+import Pagination from "../Pagination/Pagination";
+import SearchBox from "../SearchBox/SearchBox";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
 function App() {
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
+  const [debouncedSearch] = useDebounce(search, 300)
+
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["note", debouncedSearch, page],
+    queryFn: () => fetchNotes(debouncedSearch, page),
+    placeholderData: keepPreviousData,
+  });
+
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false);
+  const totalPages = data?.totalPages || 0;
 
   return (
-    <>
-    </>
-  )
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <SearchBox value={search} onSearch={setSearch} />
+        {isSuccess && data?.totalPages > 1 && (
+          <Pagination
+            totalPages={totalPages}
+            pageHandler={setPage}
+            page={page}
+          />
+        )}
+        {
+          <button className={css.button} onClick={openModal}>
+            Create note +
+          </button>
+        }
+      </header>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {isSuccess && data.notes.length > 0 && <NoteList notes={data.notes} />}
+      {isOpen && <NoteModal onClose={closeModal} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/services/noteService.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+import type { Note } from "../types/note";
+import { newNote } from "../components/NoteForm/NoteForm";
+
+interface GetNotesRes {
+  notes: Note[];
+  totalPages: number;
+}
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+const myKey = import.meta.env.VITE_NOTEHUB_TOKEN;
+const PER_PAGE = 12;
+
+const fetchNotes = async (
+  search: string,
+  page: number
+): Promise<GetNotesRes> => {
+  try {
+    const response = await axios.get<GetNotesRes>("/notes", {
+      params: {
+        page,
+        perPage: PER_PAGE,
+        ...(search !== "" && { search: search }),
+      },
+      headers: {
+        Authorization: `Bearer ${myKey}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch notes:", error);
+    throw new Error("Failed to fetch notes. Please try again later.");
+  }
+};
+
+const deleteNote = async (noteId: number) => {
+  try {
+    const response = await axios.delete(`/notes/${noteId}`, {
+      headers: {
+        Authorization: `Bearer ${myKey}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Failed to delete note:", error);
+    throw new Error("Failed to delete note. Please try again later.");
+  }
+};
+
+const createNote = async (newNote: newNote) => {
+  try {
+    const response = await axios.post<Note>("/notes", newNote, {
+      headers: {
+        Authorization: `Bearer ${myKey}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Failed to create note:", error);
+    throw new Error("Failed to create note. Please try again later.");
+  }
+};
+
+export { fetchNotes, deleteNote, createNote };
